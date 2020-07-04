edb1a868e852908688fb43e6018b582fc6059543
Alex Altn <alex.altn@gmail.com>
2020-07-04 23:58:28 +0300

    slider: оживляет слайдер

--- js/scripts.js
+++ js/scripts.js
@@ -209,4 +209,32 @@ window.onclick = function(event) {
   }
 }
 
-//dropdown menu -----------
+//Create A Slideshow -----------
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slider__item");
+  var dots = document.getElementsByClassName("slide-button");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" slide-button--active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " slide-button--active";
+}
